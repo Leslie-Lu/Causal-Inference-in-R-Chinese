6d63c51571dab544ae0b924e75b9d21988c113f5
Leslie-Lu <luzh29@mail2.sysu.edu.cn>
2024-09-03 17:16:54 +0800

    test

---
+++ etc/web.R
@@ -0,0 +1,11 @@
+# ?renv::status()
+renv::init()
+renv::status()
+renv::activate()
+renv::restore(repos = "https://cloud.r-project.org")
+renv::install(c(
+  'backports','base64enc','bslib', 'cachem','checkmate','cli','colorspace','cpp11',
+  'digest','dplyr','evaluate','fansi','farver','fastmap','fontawesome','fs','generics',
+  'ggplot2',
+))
+renv::snapshot()
