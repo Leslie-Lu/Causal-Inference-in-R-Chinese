22d2e511f362ddae9842031998dc41af038e2850
Leslie-Lu <luzh29@mail2.sysu.edu.cn>
2024-09-03 20:40:55 +0800

    update 1.1

--- etc/web.R
+++ etc/web.R
@@ -1,13 +1,13 @@
 # ?renv::status()
 # renv::init()
 renv::status()
-renv::activate()
+# renv::activate()
 # renv::restore(repos = "https://cloud.r-project.org")
 renv::restore()
-renv::install(c(
-  'backports','base64enc','bslib', 'cachem','checkmate','cli','colorspace','cpp11',
-  'digest','dplyr','evaluate','fansi','farver','fastmap','fontawesome','fs','generics',
-  'ggplot2',
-))
+# renv::install(c(
+#   'backports','base64enc','bslib', 'cachem','checkmate','cli','colorspace','cpp11',
+#   'digest','dplyr','evaluate','fansi','farver','fastmap','fontawesome','fs','generics',
+#   'ggplot2',
+# ))
 renv::snapshot()
-remove.packages("igraph")
+# remove.packages("igraph")
